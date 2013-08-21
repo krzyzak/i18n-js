84735915723c723d4eb595288839a5205f24c614
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2013-08-21 12:20:53 +0200

    Do not requre ```initialize_on_precompile``` configuration method in Rails 4

--- lib/i18n-js.rb
+++ lib/i18n-js.rb
@@ -15,7 +15,7 @@ module SimplesIdeias
     # full environment will be available during asset compilation.
     # This is required to ensure I18n is loaded.
     def assert_usable_configuration!
-      @usable_configuration ||= Rails.version >= "3.1.1" &&
+      @usable_configuration ||= Rails.version >= "4.0.0" || Rails.version >= "3.1.1" &&
         Rails.configuration.assets.initialize_on_precompile ||
         raise("Cannot precompile i18n-js translations unless environment is initialized. Please set config.assets.initialize_on_precompile to true.")
     end
